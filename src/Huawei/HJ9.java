80669d7add8d3adefb5ef4510d653b4534975705
Haozhe Chen <chz_chen@outlook.com>
2021-03-12 18:27:24 +0800

    update

--- src/Huawei/HJ9.java
+++ src/Huawei/HJ9.java
@@ -3,6 +3,15 @@ package Huawei;
 import javax.print.DocFlavor;
 import java.util.*;
 
+//题目描述
+//        输入一个int型整数，按照从右向左的阅读顺序，返回一个不含重复数字的新的整数。
+//        保证输入的整数最后一位不是0。
+//        输入描述:
+//        输入一个int型整数
+//
+//        输出描述:
+//        按照从右向左的阅读顺序，返回一个不含重复数字的新的整数
+
 public class HJ9 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
